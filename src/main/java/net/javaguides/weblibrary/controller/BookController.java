c5aeb14f549a7d208f6e7f78ee28921e5efb0ae5
Ana Špiko <anaspiko2@gmail.com>
2021-11-26 01:10:49 +0100

    Post Method Added Connecting from different local host problem fixed

--- src/main/java/net/javaguides/weblibrary/controller/BookController.java
+++ src/main/java/net/javaguides/weblibrary/controller/BookController.java
@@ -3,13 +3,17 @@ package net.javaguides.weblibrary.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.javaguides.weblibrary.model.Book;
 import net.javaguides.weblibrary.repository.BookRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class BookController {
@@ -22,5 +26,11 @@ public class BookController {
 	public List<Book> getAllBooks() {
 		return bookRepository.findAll();
 		
+	}
+	
+	//create book rest api
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		return bookRepository.save(book);
 	}
 }
